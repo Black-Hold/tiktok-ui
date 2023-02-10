14648ed4c808f7c0abf0c926cf1628bbf2da4fc9
Black-Hold <blackdiamond.DTD@gmail.com>
2023-02-10 23:30:01 +0700

    cấu hình router

---
+++ src/routes/index.js
@@ -0,0 +1,22 @@
+//Layouts
+import HeaderOnly from '@/components/Layout/HeaderOnly';
+
+
+import Home from '@/pages/Home';
+import Following from '@/pages/Following';
+import Profile from '@/pages/Profile';
+import Upload from '@/pages/Upload';
+import Search from '@/pages/Search';
+const publicRoutes = [
+    {path : '/' , comment: Home },
+    {path : '/following' , comment: Following},
+    {path : '/profile' , comment: Profile},
+    {path : '/upload' , comment: Upload, layout : HeaderOnly},
+    {path : '/search' , comment: Search, layout: null},
+
+]
+const privateRoutes = [
+    
+]
+
+export {publicRoutes , privateRoutes}
