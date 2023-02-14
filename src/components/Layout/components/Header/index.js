1d74a9462fa8d1a301d38ea6e9067deaee058932
Black-Hold <blackdiamond.DTD@gmail.com>
2023-02-15 02:01:19 +0700

    header

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,4 +1,31 @@
-function Header(){
-    return <h2> Header</h2>
+import classNames from 'classnames/bind'
+import styles from './Header.module.scss'
+import images from '@/assets/images'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons'
+
+const cx = classNames.bind(styles)
+
+function Header() {
+    return <header className={cx('wrapper')}>
+        <div className={cx('content')}>
+            <div className={cx('logo')}>
+                <img src={images.logo} alt="TIKTOK" />
+            </div>
+            <div className={cx('search')}>
+                <input placeholder='search accounts and videos' spellCheck={false} />
+                <button className={cx('search-clear')}>
+                    <FontAwesomeIcon icon={faCircleXmark} />
+                </button>
+                {/* <FontAwesomeIcon className={cx('loading')} icon={faSpinner} /> */}
+                <button className={cx('search-btn')}>
+                    <FontAwesomeIcon icon={faMagnifyingGlass} />
+                </button>
+            </div>
+            <div className={cx('actions')}>
+                login
+            </div>
+        </div>
+    </header>
 }
 export default Header
